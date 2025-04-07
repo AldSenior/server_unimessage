86ce97e99c89bfe76ababcc1e4f18a92e879d5f1
AldSenior <aldsio196@gmail.com>
2025-04-07 19:50:28 +0300

    _

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const easyvk = require("easyvk");
 const app = express();
+const axios = require("axios");
 
 app.use(express.json());
 app.use(
